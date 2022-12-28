b7bb3569cafd8d95dc5ac6c1738ca42aa00712a3
Natnael Getachew <natnaelgetachew@Natnaels-MacBook-Pro.local>
2022-12-29 00:31:09 +0300

    first commit

---
+++ app/Http/Controllers/Authcontroller.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Http\Request;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Laravel\Sanctum\HasApiTokens;
+
+class Authcontroller extends Controller
+{
+  use HasApiTokens, HasFactory, Notifiable;
+    /**
+     * create user
+    *@param  \Illuminate\Http\Request  $request
+    * @return \Illuminate\Http\Response
+     */
+    // public function __construct() {
+    //     $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    // }
+    public function createuser(Request $request)
+    {
+
+        try{
+            $uservalidator=Validator::make($request->all(),[
+                'name' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                'password' => ['required'],
+            ]);
+            if($uservalidator->fails()){
+                return response()->json([
+                    "status"=>false,
+                    "message"=>"valitor error",
+                    "error"=>$uservalidator->errors()
+                ],401);
+            }
+            $user=User::create(
+                [
+                    'name'=>$request->name,
+                    'email'=>$request->email,
+                    'password'=>Hash::make($request->password)
+                ]
+                );
+            return response()->json([
+                "status"=>true,
+                "message"=>"user created succesfully",
+                "token"=>$user->createtoken("API TOKEN")->plainTextToken
+            ] ,200);
+        }
+        catch(\Throwable $th){
+            return response()->json([
+                "status"=>true,
+                "message"=>$th->getMessage()
+            ],500);
+
+        }
+
+
+    }
+    public function loginuser(Request $request)
+    {
+        try{
+            $uservalidator=Validator::make($request->all(),[
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                'password' => ['required']
+              ]);
+              if(!Auth::attempt($request->only('email','password'))){
+
+                return response()->json([
+                    "status"=>false,
+                    "message"=>"Email or password error"
+                ],401);
+              }
+        $user=User::where("email",$request->email)->first();
+        return response()->json([
+            "status"=>true,
+            "message"=>"user logined succesfully",
+            "token"=>$user->createtoken("API TOKEN")->plainTextToken
+        ] ,200);
+
+        }catch(\Throwable $th){
+            return response()->json([
+                "status"=>true,
+                "message"=>$th->getMessage()
+            ],500);
+
+        }
+
+    }
+    public function logout() {
+        auth()->logout();
+        return response()->json(['message' => 'User successfully signed out']);
+    }
+}
---
+++ app/Http/Controllers/Post.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\storepostrequest;
+use App\Models\Post as ModelsPost;
+use Illuminate\Http\Request;
+
+class Post extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $Post=ModelsPost::all();
+         return response()->json([
+            'status'=>true,
+            'posts'=>$Post
+         ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(storepostrequest $request)
+    {
+         $posts=ModelsPost::create($request->all());
+           return response()->json([
+            "status"=>true,
+            "message"=>"post created succesfully",
+            "post"=>$posts
+           ],200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request,ModelsPost $Post)
+    {
+        $Post->update($request->all());
+        return response()->json([
+            "status"=>true,
+            "message"=>"post updated succesfully",
+            "posts"=>$Post
+
+        ],200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ModelsPost $Post)
+    {
+        $Post->delete();
+        return response()->json([
+            "status"=>true,
+            "message"=>"post delete succesfully"
+        ],200);
+    }
+}
