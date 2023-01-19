7d763c0fbd158cd5afa5e0dda4e51258242cc0b0
Natnael Getachew <natnaeln4d@gmail.com>
2023-01-19 15:31:25 +0300

    adding app/Http/Controllers/CodeCheckController.php

---
+++ app/Http/Controllers/CodeCheckController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ResetPassword;
+use Illuminate\Http\Request;
+
+class CodeCheckController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        $request->validate([
+            'code' => 'required|string|exists:reset_code_passwords',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        // find the code
+        $passwordReset = ResetPassword::firstWhere('code', $request->code);
+
+        // check if it does not expired: the time is one hour
+        if ($passwordReset->created_at > now()->addHour()) {
+            $passwordReset->delete();
+            return response(['message' => trans('passwords.code_is_expire')], 422);
+        }
+
+        // find user's email
+        $user = User::firstWhere('email', $passwordReset->email);
+
+        // update user password
+        $user->update($request->only('password'));
+
+        // delete current code
+        $passwordReset->delete();
+
+        return response(['message' =>'password has been successfully reset'], 200);
+    }
+}
